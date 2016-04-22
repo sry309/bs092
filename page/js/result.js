1d82eebacbaf8f11559b0e92f7373ebb5c1c04a1
wizardforcel <562826179@qq.com>
2016-04-22 16:52:36 +0800

    2016-04-22 16:52:36

--- page/js/result.js
+++ page/js/result.js
@@ -4,6 +4,21 @@ $(function() {
     var type = localStorage.getItem('resultType');
     var rsrc = localStorage.getItem('resultRsrc');
     $('#rsrc-name').text(rsrc);
+    
+    $resultHeadTr = $('#result-head');
+    if(type == 'assoc')
+    {
+        $('<th>关联规则</th>').appendTo($resultHeadTr);
+        $('<th>支持度</th>').appendTo($resultHeadTr);
+    }
+    else if(type == 'classify' || type == 'cluster')
+    {
+        $('<th>id</th>').appendTo($resultHeadTr);
+        $('<th>原始数据</th>').appendTo($resultHeadTr);
+        $('<th>标签</th>').appendTo($resultHeadTr);
+    }
+    else
+        throw new Error();
 
     
     var getResult = function() {
@@ -15,62 +30,43 @@ $(function() {
             if (!json.succ) 
                 alert(json.errmsg);
             else
-                getData(json.data);
+                loadResult(json.data);
         }).fail(function(data) {
             alert('Network error!');
         });
     };
     
-    var getData = function(result) {
-        
-        var rsrcName = rsrc.split('/')[2];
-        var url = getUrl() + '/Entity/' + rsrc + '/';
-        $.ajax({
-            type: "GET",
-            url: url,
-            dataType: "json"
-        }).done(function(json) {
-            if (!json[rsrcName]) 
-                alert('Network error!');
-            else
-                loadResult(result, json[rsrcName]);
-        }).fail(function(data) {
-            alert('Network error!');
-        });
-    };
     
-    var loadResult = function(result, data) {
+    var loadResult = function(list) {
         
-        /*// deal with data
-        if(data.length == 0)
-            throw new Error();
-        
-        var keys = {}
-        for(var k in data[0]);
-            
-        
-        var dataMap = {}
-        for(var i = 0; i < data.length; i++)
-        {
-            var elem = data[i];
-            var id = elem.id;
-            delete elem.id;
-            dataMap[id] = elem;
-        }
         
         $('.result-row').remove();
         for (var i = 0; i < list.length; i++) {
             var elem = list[i];
             
             var $tr = $('<tr class="result-row"></tr>')
-            var $idTd = $('<td>' + elem[0] + '</td>');
-            var $labelTd = $('<td>' + elem[1] + '</td>');
             
-            $tr.append($idTd);
-            $tr.append($labelTd);
+            if(type == 'assoc')
+            {
+                var $ruleTd = $('<td>' + elem[2] + '</td>');
+                var $confTd = $('<td>' + elem[1] + '</td>');
+                $tr.append($ruleTd);
+                $tr.append($confTd);
+            }
+            else if(type == 'cluster' || type == 'classify')
+            {
+                var $idTd = $('<td>' + elem[0] + '</td>');
+                var $dataTd = $('<td>' + elem[2] + '</td>');
+                var $labelTd = $('<td>' + elem[1] + '</td>');
+                $tr.append($idTd);
+                $tr.append($dataTd);
+                $tr.append($labelTd);
+            }
+            else
+                throw new Error();
+
             $('#result-table').append($tr);
-            
-        }*/
+        }
     };
     
     getResult();
