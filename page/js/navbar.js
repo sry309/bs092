31b40aaf9468917d277b2e7cbd0f6897c6e1e8d9
wizardforcel <562826179@qq.com>
2016-04-28 16:07:24 +0800

    2016-04-28 16:07:24

---
+++ page/js/navbar.js
@@ -0,0 +1,16 @@
+$(function()
+{
+
+    if(getUser())
+    {
+      $('.navbar-nav').append('<li><a href="./project.html">项目</a></li>');
+      $('.navbar-nav').append('<li><a href="./history.html">历史</a></li>');
+      $('.navbar-nav').append('<li><a href="./logout.html">退出</a></li>');
+    }
+    else
+    {
+      $('.navbar-nav').append('<li><a href="./login.html">登录</a></li>');
+      //$('.navbar-nav').append('<li><a href="./reg.html">注册</a></li>');
+    }
+    
+});
